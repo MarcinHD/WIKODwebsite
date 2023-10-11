f4ff5e54aec46d9c6990a7daff41da9008da8f9b
MarcinHD <MarcinHD@proton.me>
2023-10-11 11:54:22 +0200

    Playing around with bootstrap

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 import express from "express";
-import bodyParser from "body-parser";
+import body from "body-parser";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 
 const app = express();
 const port = 3000;
 
+app.use(express.static("public"));
+app.use(body.urlencoded({extended: true}));
+
+var data = {
+    year: 2023,
+    bandAdj: "",
+    bandNoun: "",
+  }
+
+
+
 app.get("/", (req,res) => {
-    res.send("./index.html");
+    // res.sendFile(__dirname + "/public/index.html");
+    res.render("index.ejs",data);
 });
 
 app.listen(port, () => {
