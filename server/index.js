94c6c666ab712ed3ffc05bd0867b6c863c30ecc6
MarcinHD <MarcinHD@proton.me>
2023-10-23 12:52:25 +0200

    Added logging feature - work in progress

--- server/index.js
+++ server/index.js
@@ -1,17 +1,44 @@
+// <-- IMPORT -->
+import dotenv from "dotenv";
 import express from "express";
 import body from "body-parser";
 import path from "path";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-const __dirname = dirname(fileURLToPath(import.meta.url));
+import mongoose from "mongoose";
+import session from "express-session";
+import passport from "passport";
+import passportLocalMongoose from "passport-local-mongoose";
 
+// <-- INITIALIZE -->
+dotenv.config();
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 5000;
 
 app.use(express.static("public"));
 app.use(body.urlencoded({extended: true}));
+app.use(session({ secret: process.env.SECRET_KEY, resave: false, saveUninitialized: false }));
+app.use(passport.initialize());
+app.use(passport.session());
 
+// <-- DATABASE SETTING -->
+mongoose
+    .connect("mongodb://127.0.0.1:27017/Secrets", {useNewUrlParser: true})
+    .then( () => console.log("connected to DB."))
+    .catch( err => console.log(err + "\ncant connect to DB ..."));
+
+const userSchema = new mongoose.Schema({
+    username: String,
+    password: String
+});
+userSchema.plugin(passportLocalMongoose);
+const User = new mongoose.model("User",userSchema);
+
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
   // < -- EXPRESS ROUTER --> 
 app.get("/home", (req,res) => {
@@ -32,11 +59,47 @@ app.get("/contact", (req,res) => {
     res.render("contact.ejs",data);
 });
 app.get("/login", (req,res) => {
-    res.render("login.ejs")
+    if(req.isAuthenticated()){
+        res.redirect("/list");
+    } else{
+        res.render("login.ejs");
+    }
+});
+app.post("/login", (req,res) => {
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
+    });
+    req.login(user, function(err){
+        if(err){
+            console.log(err);
+            res.redirect("/login");
+        } else{
+            passport.authenticate("local")(req,res,function(){
+                res.redirect("/list");
+            });
+        }
+    });
 });
 
 app.get("/signup", (req,res) => {
-    res.render("signup.ejs")
+    if(req.isAuthenticated()){
+        res.redirect("/list");
+    } else{
+        res.render("signup.ejs");
+    }
+});
+app.post("/signup", (req,res) => {
+    User.register({username: req.body.username}, req.body.password, function(err,user){
+        if(err){
+            console.log(err);
+            res.redirect("/signup");
+        } else {
+            passport.authenticate("local")(req,res,function(){
+                res.redirect("/list");
+            });
+        }
+    });
 });
 app.all("/", function(req, res) {
     res.redirect("http://localhost:"+port+"/home");
@@ -46,8 +109,11 @@ app.all("/", function(req, res) {
 app.use(express.static(path.join(__dirname, "..", "client", "build")));
 
 app.get("/list", (req,res) => {
-    console.log("List");
-    res.sendFile(path.join(__dirname,"..","client","build","index.html"));
+    if(req.isAuthenticated()){
+        res.sendFile(path.join(__dirname,"..","client","build","index.html"));
+    } else{
+        res.redirect("/login");
+    }
 });
 
   // < -- REDIRECT REST --> 
@@ -59,6 +125,7 @@ app.listen(port, () => {
     console.log("Server started on port:" + port);
 });
 
+// <-- DATA AND FUNCTIONS -->
 const data ={
     port: port,
     activePage: [
