c03804576d751d7fc59f6710ac910e26244dc9e5
MarcinHD <MarcinHD@proton.me>
2023-10-26 17:31:13 +0200

    Create Outlets

--- client/src/Outlets/Home.js
+++ client/src/Outlets/Home.js
@@ -1,8 +1,45 @@
-
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
 
 function Home(){
     return (
-        <h1>Home</h1>
+        <Grid container spacing={3}>
+
+        <Grid item xs={12} md={8} lg={9}>
+          <Paper
+            sx={{
+              p: 2,
+              display: 'flex',
+              flexDirection: 'column',
+              height: 240,
+            }}
+          >
+          <img className="img-fluid" 
+                src={`${process.env.PUBLIC_URL}/images/dashboard.jpg`} 
+                alt="logo"
+                height={210}
+                />
+          </Paper>
+        </Grid>
+
+        <Grid item xs={12} md={4} lg={3}>
+          <Paper
+            sx={{
+              p: 2,
+              display: 'flex',
+              flexDirection: 'column',
+              height: 240,
+            }}
+          >
+          </Paper>
+        </Grid>
+
+        <Grid item xs={12}>
+          <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column' }}>
+
+          </Paper>
+        </Grid>
+      </Grid>
     );
 }
 
