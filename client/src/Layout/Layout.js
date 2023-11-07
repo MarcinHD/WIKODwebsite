c4822042ed3005739f1e4737ef0574bf3ac9785d
MarcinHD <MarcinHD@proton.me>
2023-11-07 12:37:24 +0100

    Rebuild layout working schema -part1

--- client/src/Layout/Layout.js
+++ client/src/Layout/Layout.js
@@ -20,16 +20,40 @@ import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 import NotificationsIcon from '@mui/icons-material/Notifications';
 import MenuList from './MenuList';
 import Header from './Header';
+import Orders from '../Outlets/Orders';
+import Home from '../Outlets/Home';
+import Discounts from '../Outlets/Discounts';
+import Products from '../Outlets/Products';
+import History from '../Outlets/History';
   
 const defaultTheme = createTheme();
 
 function Layout(){
   const [showAlert, setShowAlert] = React.useState(false);
+  const [page, setPage] = React.useState(0);
+
+  function switchPage(){
+    switch(page){
+      case 0:
+        return <Home/>
+      case 1:
+        return <Orders />;
+      case 2:
+        return <Discounts />;
+      case 3:
+        return <Products />;
+      case 4:
+        return <History />;
+      default:
+        return <Home/>
+    }
+  }
+
   return (
     <ThemeProvider theme={defaultTheme}>
       <Box sx={{ display: 'flex' }}>
         <CssBaseline />
-        <Header openMenu={setShowAlert}/>
+        <Header openMenu={setShowAlert} page={setPage}/>
         <Box
           component="main"
           sx={{
@@ -45,7 +69,8 @@ function Layout(){
           <Toolbar />
           <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
             <Grid container spacing={3}>
-                <Outlet />
+                {switchPage()}
+                {/* <Outlet /> */}
             </Grid>
             <Footer sx={{ pt: 4 }} />
             <Collapse in={showAlert}>
